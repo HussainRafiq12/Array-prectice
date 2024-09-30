50ead9f04a6d481b6e2ecf47d0d0a448f3d65aa0
Hussain Rafiq <160489258+HussainRafiq12@users.noreply.github.com>
2024-09-30 18:14:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+// var Arr = [1,3, 10 ,2 ,5 ,9]
+
+// for (let i = 1; i < Arr.length; i++){
+//     for (j = 0; j < i; j++){
+//         if (Arr[i] < Arr[j]){
+//          var x = Arr [i];
+//          Arr[i] = Arr[j];
+//          Arr[j] = x;
+//         }
+//     }
+//     console.log(Arr)
+// }
+
+// let all = [1,2,3,4,5]
+
+// for (let i = 0; i < all.length; i++ ){
+//     console.log(all[i]);
+// }
+
+
+// sum of all elements in an array
+// let arr = [1,2,3,4,5]
+// let sum = 0;
+// for (let i = 0;i <arr.length;i++){
+//     sum += arr[i];
+  
+// }
+// console.log(sum)
+
+// find the Maximum Element in an array
+// let arr = [5,2,9,1,7,4]
+// let max =  arr[0];
+
+// for (let i = 1; i <arr.length; i++){
+//     if (arr[i] > max ){
+//         max = arr[i ];
+//     }
+// }
+// console.log(max)
+
+
+// var temperatures = [100,90,99,80,70,65,30,10];
+
+//   for (var i = 0; i < temperatures.length; i++) {
+//     console.log(temperatures[i]);
+//   }
+
+
+// let largestIndex = 0;
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > arr[largestIndex]) {
+//         largestIndex = i;
+//     }
+ 
+// }
+// console.log(largestIndex)
+
+// let arr = [3,5,8,6,10,15,1]
+// let max = [0]
+// for (let i = 1; i <arr.length; i++){
+//   for (let j = 15; j > max; j--){
+//     max = arr[i];
+//   }
+
+//     console.log(max)
+// }
+
+// let numbers = [2,5,8,6,9,13,]
+// let largest = numbers[0];
+
+// for(let i = 1; i <numbers.length; i++){
+//     if (numbers[i] < largest){
+//         largest = numbers[i];
+//     }
+// }
+
+// console.log("The largest number is:", largest);
+
+// let numbers = [3,2,5,6,10,12,89]
+
+
+// for (let i = 0; i < 5; i++){
+//     console.log(numbers[i])
+// }
+
+// console.log("The largest number is:", largest);
+
+// let numbers = [3,2,5,6,10,12,89]
+
+
+// for (let i = numbers.length -5; i < 7; i++){
+//     console.log(numbers[i])
+// }
+// let numbers =[2,10,23,34,65,87,90,12]
+// let evenNumbers =[];
+
+// for (let i = 0; i <numbers.length; i++ ){
+//  if(numbers[i] % 2 !== 0){
+//     evenNumbers.push(numbers[i])
+// }
+// }
+// console.log("even numbers", evenNumbers);
+
+// let arr = [4,6,8,2,9,10,34,78,23,89];
+
+// for (i = 0; i <arr.length; i++){
+//     for (let j = 0; j <arr.length - 1- i; j++){
+//     if (arr[j] < arr[j+1]){
+//        let same = arr[j];
+//        arr[j]  = arr[j+1]
+//        arr[j+1] = same;
+//     }
+//     }
+// }
+// console.log(arr)
+
+
